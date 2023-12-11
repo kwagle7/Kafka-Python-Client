aaa5a3e63e65a44f788b8c53f335f76a6a5f1cab
Kshitij Wagle <kshitij.wagle1@gmail.com>
2023-12-11 15:18:18 +1100

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,10 +1,21 @@
+#kwagle
+
+# Importing the KafkaConsumer class from the kafka library
 from kafka import KafkaConsumer
+
+# Creating a KafkaConsumer instance for the 'bankbranch' topic with specific configurations
 consumer = KafkaConsumer('bankbranch',
-                        group_id=None,
-                         bootstrap_servers=['localhost:9092'],
-                         auto_offset_reset = 'earliest')
+                        group_id=None,  # Setting group_id to None
+                        bootstrap_servers=['localhost:9092'],  # Kafka broker address
+                        auto_offset_reset='earliest')  # Setting auto offset to earliest
+
+# Printing 'Hello' to the console
 print("Hello")
+
+# Printing information about the KafkaConsumer object (not the messages)
 print(consumer)
 
+# Iterating through messages received by the consumer indefinitely
 for msg in consumer:
+    # Printing the value of the received message after decoding it from bytes to UTF-8 string
     print(msg.value.decode("utf-8"))
